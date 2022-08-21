d4323a6debb0618531019249edd2b5ece81c5d36
Enzosanchezz <enzosanchez771@gmail.com>
2022-08-21 03:27:59 -0300

    landing

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -10,15 +10,12 @@ export default function Landing(){
             <span className={style.landing}>
                 <h1>Bienvendidos! 😊</h1>
                 <h3>Hola mi nombre es Enzo 👋🏻, antes de clickear el enlace "Comenzar" déjame contarte un poco de lo que se trata este proyecto.</h3>
-                <h4>Es una app sobre Pokemons traidos desde una api externa y almacenadops en una base de datos.
-                    El objetivo era armar el back-end y el front-end, conectarlos y darle un poco de estilo con css.
-                    Ahora que ya sabes mas o menos de que se trata, clickea 👉🏻 <Link className={style.link} to="/home">
-                    Comenzar
-                </Link> y disfruta la app!! 
+                <h4>Desarrolle una SPA(Single Page Application) utilizando React para el Front End y Redux cómo stmanagement. Todos los componentes fueron desarrollados con CSS sin uso de librerías externas. 
+                La SPA consume datos de una API a través de un Back End desarrollado en Node.JS utilizando Express, agregando nuevas funcionalidades a la API original.
+                Algunos features del proyecto: ordenamientos, filtros, busquedas, añadir a favoritos, formulario controlado para la creación de Pokemons formulario controlado para actualizar Pokemons.
+                Ahora si clickea <Link className={style.link} to="/home">Comenzar</Link> y disfruta la app!! 
                 </h4>
-                
             </span>
-            
         </div>
     )
 }
