8df20e9adcc50c4fe292360fcbb492e00017c739
Enzosanchezz <enzosanchez771@gmail.com>
2023-02-15 10:35:25 -0300

    agregando server a .env

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 import Swal from "sweetalert2";
-
-const SERVER = "https://api-poke.up.railway.app"
-// const SERVER = "http://localhost:3001"
+const {REACT_APP_SERVER} = process.env;
 
 export function getPokemons(){
     return async function(dispatch){
-        let all = await axios.get(`${SERVER}/pokemons`)
+        let all = await axios.get(`${REACT_APP_SERVER}/pokemons`)
         return dispatch({type: "GET_POKEMONS", payload: all.data})
     }
 }
@@ -14,7 +12,7 @@ export function getPokemons(){
 export function getNamePokemons(name){
     return async function(dispatch){
         try {
-            let pokName = await axios.get(`${SERVER}/pokemons?name=${name}`) 
+            let pokName = await axios.get(`${REACT_APP_SERVER}/pokemons?name=${name}`) 
             return dispatch({type : "GET_NAME_POKEMON", payload: pokName.data})
         } catch (error) {
             Swal.fire({
@@ -29,7 +27,7 @@ export function getNamePokemons(name){
 
 export function getType(){
     return async function(dispatch){
-        let json = await axios.get(`${SERVER}/types`)
+        let json = await axios.get(`${REACT_APP_SERVER}/types`)
         return dispatch({
             type: 'GET_TYPE',
             payload : json.data
@@ -39,21 +37,21 @@ export function getType(){
 
 export function detailPoke(id){
     return async function(dispatch){
-        let details = await axios.get(`${SERVER}/pokemons/${id}`)
+        let details = await axios.get(`${REACT_APP_SERVER}/pokemons/${id}`)
         return dispatch({type : "GET_DETAILS", payload: details.data})
     }
 }
 
 export function postPokemon(payload){
     return async function(dispatch){
-        let data = await axios.post(`${SERVER}/pokemons`, payload)
+        let data = await axios.post(`${REACT_APP_SERVER}/pokemons`, payload)
         return dispatch({type : "POST_POKEMON", data})
     }
 }
 
 export function putPokemon(id, payload){
     return async function(dispatch){
-        let putPoke = await axios.put(`${SERVER}/pokemons/${id}`, payload)
+        let putPoke = await axios.put(`${REACT_APP_SERVER}/pokemons/${id}`, payload)
         return dispatch({type: "PUT_POKEMON", putPoke})
     }
 }
